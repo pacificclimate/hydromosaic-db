a16d282b398857c0ca0843f61221a2b6261efcf3
Lee Zeman <lzeman@uvic.ca>
2025-02-14 17:20:24 -0800

    format with black

--- alembic/versions/6e2d31e1e81e_initial_database_design.py
+++ alembic/versions/6e2d31e1e81e_initial_database_design.py
@@ -5,20 +5,21 @@ Revises:
 Create Date: 2025-01-16 20:19:56.436403
 
 """
+
 from typing import Sequence, Union
 
 from alembic import op
 import sqlalchemy as sa
 
 
-
 # revision identifiers, used by Alembic.
-revision: str = '6e2d31e1e81e'
+revision: str = "6e2d31e1e81e"
 down_revision: Union[str, None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
 schema_name = "hydromosaic"
+
 
 def upgrade():
     op.create_table(
@@ -26,74 +27,78 @@ def upgrade():
         sa.Column("outlet_id", sa.Integer(), nullable=False),
         sa.Column("code", sa.String(), nullable=False),
         sa.PrimaryKeyConstraint("outlet_id"),
-        schema = schema_name
-        )
-    
+        schema=schema_name,
+    )
+
     op.create_table(
         "variables",
-        sa.Column("variable_id", sa.Integer(), nullable = False),
-        sa.Column("standard_name", sa.String(), nullable = False),
+        sa.Column("variable_id", sa.Integer(), nullable=False),
+        sa.Column("standard_name", sa.String(), nullable=False),
         sa.Column("long_name", sa.String()),
         sa.Column("units", sa.String(), nullable=False),
         sa.PrimaryKeyConstraint("variable_id"),
-        schema = schema_name
-        )
+        schema=schema_name,
+    )
 
-    
     op.create_table(
         "datafiles",
-        sa.Column("datafile_id", sa.Integer(), nullable = False),
-        sa.Column("filename", sa.String(), nullable = False),
+        sa.Column("datafile_id", sa.Integer(), nullable=False),
+        sa.Column("filename", sa.String(), nullable=False),
         sa.Column("index_time", sa.Date()),
         sa.PrimaryKeyConstraint("datafile_id"),
-        schema = schema_name,
-        )
+        schema=schema_name,
+    )
 
     op.create_table(
         "models",
-        sa.Column("model_id", sa.Integer(), nullable = False), 
+        sa.Column("model_id", sa.Integer(), nullable=False),
         sa.Column("long_name", sa.String()),
-        sa.Column("short_name", sa.String(), nullable = False),
+        sa.Column("short_name", sa.String(), nullable=False),
         sa.Column("institution", sa.String()),
         sa.PrimaryKeyConstraint("model_id"),
-        schema = schema_name
-        )
+        schema=schema_name,
+    )
 
     op.create_table(
         "scenarios",
-        sa.Column("scenario_id", sa.Integer(), nullable = False),
+        sa.Column("scenario_id", sa.Integer(), nullable=False),
         sa.Column("long_name", sa.String()),
-        sa.Column("short_name", sa.String, nullable = False),
+        sa.Column("short_name", sa.String, nullable=False),
         sa.PrimaryKeyConstraint("scenario_id"),
-        schema = schema_name
-        )
-
+        schema=schema_name,
+    )
 
     op.create_table(
         "timeseries",
-        sa.Column("timeseries_id", sa.Integer(), nullable = False),
-        sa.Column("outlet_id", sa.Integer(), nullable = False),
-        sa.Column("start_time", sa.Date(), nullable = False),
-        sa.Column("end_time", sa.Date(), nullable = False),
-        sa.Column("variable_id", sa.Integer(), nullable = False),
-        sa.Column("datafile_id", sa.Integer(), nullable = False),
-        sa.Column("model_id", sa.Integer(), nullable = False),
-        sa.Column("scenario_id", sa.Integer(), nullable = False),
-        sa.Column("num_times", sa.Integer(), nullable = False),
+        sa.Column("timeseries_id", sa.Integer(), nullable=False),
+        sa.Column("outlet_id", sa.Integer(), nullable=False),
+        sa.Column("start_time", sa.Date(), nullable=False),
+        sa.Column("end_time", sa.Date(), nullable=False),
+        sa.Column("variable_id", sa.Integer(), nullable=False),
+        sa.Column("datafile_id", sa.Integer(), nullable=False),
+        sa.Column("model_id", sa.Integer(), nullable=False),
+        sa.Column("scenario_id", sa.Integer(), nullable=False),
+        sa.Column("num_times", sa.Integer(), nullable=False),
         sa.PrimaryKeyConstraint("timeseries_id"),
         sa.ForeignKeyConstraint(["outlet_id"], [f"{schema_name}.outlets.outlet_id"]),
-        sa.ForeignKeyConstraint(["variable_id"], [f"{schema_name}.variables.variable_id"]),
-        sa.ForeignKeyConstraint(["datafile_id"], [f"{schema_name}.datafiles.datafile_id"]),
+        sa.ForeignKeyConstraint(
+            ["variable_id"], [f"{schema_name}.variables.variable_id"]
+        ),
+        sa.ForeignKeyConstraint(
+            ["datafile_id"], [f"{schema_name}.datafiles.datafile_id"]
+        ),
         sa.ForeignKeyConstraint(["model_id"], [f"{schema_name}.models.model_id"]),
-        sa.ForeignKeyConstraint(["scenario_id"], [f"{schema_name}.scenarios.scenario_id"]),
-        )
+        sa.ForeignKeyConstraint(
+            ["scenario_id"], [f"{schema_name}.scenarios.scenario_id"]
+        ),
+    )
 
 
 def downgrade():
-        op.drop_table("timeseries", schema=schema_name)
-        op.drop_table("scenarios", schema=schema_name)
-        op.drop_table("models", schema=schema_name)
-        op.drop_table("datafiles", schema=schema_name)
-        op.drop_table("times", schema=schema_name)
-        op.drop_table("variables", schema=schema_name)
-        op.drop_table("outlets", schema=schema_name)
+    op.drop_table("timeseries", schema=schema_name)
+    op.drop_table("scenarios", schema=schema_name)
+    op.drop_table("models", schema=schema_name)
+    op.drop_table("datafiles", schema=schema_name)
+    op.drop_table("times", schema=schema_name)
+    op.drop_table("variables", schema=schema_name)
+    op.drop_table("outlets", schema=schema_name)
--- hydromosaic/scripts/index_directory.py
+++ hydromosaic/scripts/index_directory.py
@@ -4,16 +4,29 @@ from argparse import ArgumentParser
 from hydromosaic.indexing.index_netCDF import index_directory
 
 
-def index(): 
-#if __name__ == "__main__":
-    parser = ArgumentParser(
-        description = "Index netCDF files into hydromosaic database"
-        )
+def index():
+    # if __name__ == "__main__":
+    parser = ArgumentParser(description="Index netCDF files into hydromosaic database")
     parser.add_argument("-d", "--dsn", help="connection string for database")
-    parser.add_argument("-l", "--log_level", help='level of logging detail', default="info", choices=["debug", "info", "warning", "error", "critical"])
-    parser.add_argument("-p", "--metadata_prefix", help="prefix for GCM metadata", default="downscaling_GCM_")
+    parser.add_argument(
+        "-l",
+        "--log_level",
+        help="level of logging detail",
+        default="info",
+        choices=["debug", "info", "warning", "error", "critical"],
+    )
+    parser.add_argument(
+        "-p",
+        "--metadata_prefix",
+        help="prefix for GCM metadata",
+        default="downscaling_GCM_",
+    )
     parser.add_argument("directory", help="directory to index")
     args = parser.parse_args()
-    
-    
-    index_directory(dsn = args.dsn, directory = args.directory, log_level=args.log_level, gcm_prefix = args.metadata_prefix)
+
+    index_directory(
+        dsn=args.dsn,
+        directory=args.directory,
+        log_level=args.log_level,
+        gcm_prefix=args.metadata_prefix,
+    )
